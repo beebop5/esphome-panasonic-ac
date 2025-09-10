a1216fdbbff000acfdb45237cee9e65e15fdb403
Duncan <dsmith@dps-i.net>
2025-09-10 22:45:33 +0200

    Use TemplateSelect instead of abstract Select class
    - Import TemplateSelect from esphome.components.select.template_select
    - Use cv.declare_id(TemplateSelect) in schema instead of select.SELECT_SCHEMA
    - Create TemplateSelect instances with cg.new_Pvariable() instead of select.new_select()
    - This should resolve the abstract class instantiation error

--- components/panasonic_ac/climate.py
+++ components/panasonic_ac/climate.py
@@ -7,6 +7,7 @@ from esphome.const import (
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, climate, sensor, select, switch
+from esphome.components.select.template_select import TemplateSelect
 
 AUTO_LOAD = ["switch", "sensor", "select"]
 DEPENDENCIES = ["uart"]
@@ -29,10 +30,12 @@ VERTICAL_SWING_OPTIONS = ["swing", "auto", "up", "up_center", "center", "down_ce
 CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(PanasonicACWLAN),
-        cv.Optional(CONF_HORIZONTAL_SWING_SELECT): select.SELECT_SCHEMA.extend({
+        cv.Optional(CONF_HORIZONTAL_SWING_SELECT): cv.Schema({
+            cv.GenerateID(): cv.declare_id(TemplateSelect),
             cv.Required("name"): cv.string,
         }),
-        cv.Optional(CONF_VERTICAL_SWING_SELECT): select.SELECT_SCHEMA.extend({
+        cv.Optional(CONF_VERTICAL_SWING_SELECT): cv.Schema({
+            cv.GenerateID(): cv.declare_id(TemplateSelect),
             cv.Required("name"): cv.string,
         }),
         cv.Optional(CONF_OUTSIDE_TEMPERATURE): sensor.sensor_schema(
@@ -54,12 +57,16 @@ async def to_code(config):
 
     if CONF_HORIZONTAL_SWING_SELECT in config:
         conf = config[CONF_HORIZONTAL_SWING_SELECT]
-        swing_select = await select.new_select(conf, options=HORIZONTAL_SWING_OPTIONS)
+        swing_select = cg.new_Pvariable(conf[CONF_ID], TemplateSelect)
+        cg.add(swing_select.set_name(conf["name"]))
+        cg.add(swing_select.set_options(HORIZONTAL_SWING_OPTIONS))
         cg.add(var.set_horizontal_swing_select(swing_select))
 
     if CONF_VERTICAL_SWING_SELECT in config:
         conf = config[CONF_VERTICAL_SWING_SELECT]
-        swing_select = await select.new_select(conf, options=VERTICAL_SWING_OPTIONS)
+        swing_select = cg.new_Pvariable(conf[CONF_ID], TemplateSelect)
+        cg.add(swing_select.set_name(conf["name"]))
+        cg.add(swing_select.set_options(VERTICAL_SWING_OPTIONS))
         cg.add(var.set_vertical_swing_select(swing_select))
 
     if CONF_OUTSIDE_TEMPERATURE in config:
